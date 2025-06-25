1a71421bcc8512bf5597e70ee28dd9d1d4e6a7dc
fratello@Ohimesama <fratello@Ohimesama.localdomain>
2025-06-25 11:00:40 +0700

    profiles app update

--- gov_security/profiles/views.py
+++ gov_security/profiles/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from profiles.models import Employee
 from django.contrib.auth.decorators import login_required
-# from django.http import HttpResponseRedirect
 
 
 @login_required
