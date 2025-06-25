5bc41fab94ab0f515dc081ec19dd1a3abe3eb0d6
fratello@Ohimesama <fratello@Ohimesama.localdomain>
2025-06-25 11:09:22 +0700

    requirements.txt add

--- gov_security/gov_security/urls.py
+++ gov_security/gov_security/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for gov_security project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -31,4 +15,5 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/accounts/login/', permanent=True)),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
